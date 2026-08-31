4bb2240e73292f2dab4c202859b45362a8852df8
chiomegadeveloper-svg <chiomegadeveloper@gmail.com>
2026-08-31 21:24:41 +0800

    Polish map layout and orange create action

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "picklester-v25-transparent-logo";
+const CACHE = "picklester-v26-map-layout";
 self.addEventListener("install", (event) => event.waitUntil(Promise.all([
   caches.open(CACHE).then((cache) => cache.addAll(["/", "/manifest.webmanifest", "/picklester-logo-transparent.png", "/icon-192.png", "/icon-512.png"])),
   self.skipWaiting(),
