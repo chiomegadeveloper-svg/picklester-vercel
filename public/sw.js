a46c54dc1371f2076539632180135bd28ddc3369
chiomegadeveloper-svg <chiomegadeveloper@gmail.com>
2026-08-31 22:10:28 +0800

    Fix four-second splash and add production export

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "picklester-v27-honesty-controls";
+const CACHE = "picklester-v28-four-second-logo";
 self.addEventListener("install", (event) => event.waitUntil(Promise.all([
   caches.open(CACHE).then((cache) => cache.addAll(["/", "/manifest.webmanifest", "/picklester-logo-transparent.png", "/icon-192.png", "/icon-512.png"])),
   self.skipWaiting(),
