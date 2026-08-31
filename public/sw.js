902929e5bcab244b4479326ab7f1a4e4a9c51ac0
chiomegadeveloper-svg <chiomegadeveloper@gmail.com>
2026-08-31 22:37:16 +0800

    Expose and enforce honesty mode games

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "picklester-v28-four-second-logo";
+const CACHE = "picklester-v29-honesty-response";
 self.addEventListener("install", (event) => event.waitUntil(Promise.all([
   caches.open(CACHE).then((cache) => cache.addAll(["/", "/manifest.webmanifest", "/picklester-logo-transparent.png", "/icon-192.png", "/icon-512.png"])),
   self.skipWaiting(),
